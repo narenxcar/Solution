8b993abd247dfd1995ca8eb4f97cecacc34b5b04
narenxcar <98880549+narenxcar@users.noreply.github.com>
2022-02-07 20:11:49 +0530

    sample

--- Power.java
+++ Power.java
@@ -1,13 +1,32 @@
 import java.util.*;
 public class Power {
-
+	static double power(double x,int num) {
+		double ans=1;
+		int n=num;
+		if(num<0) {
+			n=-1 * n;
+		}
+		while(n>0) {
+			if(n%2==0) {
+				x=x*x;
+				n=n/2;
+			}else {
+				ans=ans*x;
+				n=n-1;
+			}
+		}
+		if(num<0) {
+			return  1.0/ans;
+		}
+		return ans;
+	}
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		System.out.print("Enter the Base value : ");
 		double b=input.nextDouble();
 		System.out.print("\nEnter the Power value : ");
-		double p=input.nextDouble();
-		double c=Math.pow(b,p);
+		int p=input.nextInt();
+		double c=power(b,p);
 		System.out.println(b+" Power "+p+" is "+c);
 	}
 
