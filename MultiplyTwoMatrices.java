173f2edd119a9c056518ea34c4c71614b5e1313e
narenxcar <98880549+narenxcar@users.noreply.github.com>
2022-02-08 13:17:11 +0530

    sample

--- MultiplyTwoMatrices.java
+++ MultiplyTwoMatrices.java
@@ -15,16 +15,20 @@ public class MultiplyTwoMatrices {
 			for(int j=0;j<3;j++){
 				arr2[i][j]=input.nextInt();
 			}
-		}
+		}int temp,h=0;
+		int arr3[][]=new int[3][3];
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				arr1[i][j]*=arr2[i][j];
+				for(int k=0;k<3;k++) {
+					arr3[i][j]+=arr1[i][k]*arr2[k][j];
+				}
 			}
+			
 		}
-		System.out.println("Two Arrays are Multiplied");
+		System.out.println("Two Matrix are Multiplied");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				System.out.print(arr1[i][j]+" ");
+				System.out.print(arr3[i][j]+" ");
 			}
 			System.out.println();
 		}
--- MultiplytwoMatricesUsingFunction.java
+++ MultiplytwoMatricesUsingFunction.java
@@ -1,12 +1,15 @@
 import java.util.*;
 public class MultiplytwoMatricesUsingFunction {
 	static int[][] matirxmultiply(int arr1[][],int arr2[][]){
+		int arr3[][]=new int[3][3];
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
-				arr1[i][j]*=arr2[i][j];
+				for (int k=0;k<3;k++) {
+					arr3[i][j]+=arr1[i][k]*arr2[k][j];
+				}
 			}
 		}
-		return arr1;
+		return arr3;
 	}
 	public static void main(String[] args) {
 		Scanner input=new Scanner (System.in);
@@ -25,7 +28,7 @@ public class MultiplytwoMatricesUsingFunction {
 			}
 		}
 		int arr3[][]=matirxmultiply(arr1,arr2);
-		System.out.println("Two Arrays are Multiplied by using method");
+		System.out.println("Matirx Multiplied by using method");
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				System.out.print(arr3[i][j]+" ");
